ffa0f5e8587da3320284a83efe3da5f8c5c1c33b
Nafisa Tabassum <nafisatabassum545@gmail.com>
2026-02-15 20:07:25 +0600

    Add basic C program to print variables

---
+++ q07_230059.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    float b = 3.5;
+    char c = 'Z';
+
+    printf("Integer: %d\n", a);
+    printf("Float: %f\n", b);
+    printf("Char: %c\n", c);
+
+    return 0;
+}
